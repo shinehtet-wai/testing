7cbcd06f98d96fbed46cf586d9c8686e7e0fd304
shinehtet-wai <>
2020-09-02 20:24:39 +0630

    first commit

---
+++ resources/views/backend/orders/show.blade.php
@@ -0,0 +1,50 @@
+{{-- @extends('backendtemplate')
+@section('content')
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 mb-3">
+			<h1 class="h3 mb-0 text-gray-800">Voucherno:{{$order->voucherno}}</h1>
+			<h1 class="h3 mb-0 text-gray-800">Orderdate:{{$order->orderdate}}</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table table-bordered">
+				<thead class="thead-dark">
+					<tr>
+						<th>No</th>
+						<th>Item Name</th>
+						<th>Price</th>
+						<th>Qty</th>
+						<th>Subtotal</th>
+					</tr>
+					</thead>
+						<tbody>
+							@php $i; $total=0;@endphp
+							@foreach($order->items as $item)
+							@php
+							$subtotal=$item->price*$item->pivot->qty;
+							$total+=$subtotal;
+							@endphp
+							<tr>
+								<td>{{$i++}}</td>
+								<td>{{$item->name}}</td>
+								<td>{{$item->price}}</td>
+								<td>{{$item->pivot->qty}}</td>
+								<td>
+									{{$subtotal}}
+								</td>
+							</tr>
+								@endforeach
+								<tr class="btn-dark text-white">
+									<td colspan="4">Total:</td>
+									<td>{{$total}}MMK</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+</div>
+@endsection --}}
